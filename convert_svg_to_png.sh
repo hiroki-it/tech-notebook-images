69fe0c3c159bf9b2547cd0475352b83bacb49220
hiroki-it <hasegawafeedshop@gmail.com>
2023-07-01 02:16:06 +0900

    add SVGファイルをPNGに変換できるようにした。

--- convert_svg_to_png.sh
+++ convert_svg_to_png.sh
@@ -2,6 +2,12 @@
 
 DIR=./images/drawio/blog/terraform
 
+# svgをpngに変換する
 for file in `find ${DIR} -name "*.svg"`; do
   qlmanage -t -s 2048 -o ${DIR} $file
+done
+
+# 拡張子からsvgを削除する
+for file in `find ${DIR} -name "*.svg.png"`; do
+  rename "s/svg.png/png/;" $file
 done
